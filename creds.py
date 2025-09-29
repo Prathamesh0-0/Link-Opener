a0fcb086dc4a89f5565fb67ebdaf50c9c379f1c4
Prathamesh0-0 <tikle.prathamesh@gmail.com>
2025-09-29 14:19:13 +0530

    iteration

--- creds.py
+++
@@ -1,2 +0,0 @@
-url_to_open = ("https://aiskillshouse.com/student/qr-mediator.html?uid=6592&promptId=17")
-repeat_count = 100
--- main.py
+++ main.py
@@ -2,10 +2,9 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import time
 from concurrent.futures import ThreadPoolExecutor
-from creds import *
 
-# Function to launch browser in incognito mode (no cache)
-def open_link_fresh(url, wait_time=10):
+
+def open_link_fresh(url, wait_time=15):
     options = Options()
     options.add_argument("--incognito")
     options.add_argument("--disable-application-cache")
@@ -15,13 +14,15 @@ def open_link_fresh(url, wait_time=10):
     time.sleep(wait_time)
     driver.quit()
 
+url_to_open = ("https://aiskillshouse.com/student/qr-mediator.html?uid=6592&promptId=17")
+repeat_count = 10
 
-# Open multiple browser sessions in parallel
-with ThreadPoolExecutor(max_workers=repeat_count) as executor:
-    futures = [
-        executor.submit(open_link_fresh, url_to_open, 10)
-        for _ in range(repeat_count)
-    ]
-    for i, f in enumerate(futures):
-        f.result()  # Waits for completion
-        print("Executed", i + 1)
+for _ in range(repeat_count):
+    with ThreadPoolExecutor(max_workers=repeat_count) as executor:
+        futures = [
+            executor.submit(open_link_fresh, url_to_open, 15)
+            for _ in range(repeat_count)
+        ]
+        for i, f in enumerate(futures):
+            f.result
+            print("Executed", i + 1)
