8810f87088b08cfa03dfba9ff9b3cd6c71798615
Prathamesh0-0 <tikle.prathamesh@gmail.com>
2025-09-29 12:31:15 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import time
+
+# Function to launch browser in incognito mode (no cache)
+def open_link_fresh(url, wait_time=5):
+    options = Options()
+    options.add_argument("--incognito")
+    options.add_argument("--disable-application-cache")
+    options.add_argument("--no-sandbox")
+    driver = webdriver.Chrome(options=options)
+    driver.get(url)
+    time.sleep(wait_time)  # Wait for the page to load completely
+    driver.quit()
+
+url_to_open = str(input("Enter the URL to open: "))
+repeat_count = int(input("Enter the number of times to repeat: "))
+
+for _ in range(repeat_count):
+    open_link_fresh(url_to_open, wait_time=5)
+    time.sleep(2)
+    print("Executed ", _+1)
